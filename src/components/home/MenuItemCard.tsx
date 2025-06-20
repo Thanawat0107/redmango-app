1391a90240213709e2d61c7ee7d4943570ff0308
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-06-20 11:01:19 +0700

    Section 01 : Fetch Menu Items  & Section 01 : Card Component

--- src/components/home/MenuItemCard.tsx
+++ src/components/home/MenuItemCard.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import menuItemModel from "../../interfaces";
 
-const MenuItemCard = () => {
+interface Props {
+  menuItem: menuItemModel;
+}
+const MenuItemCard = (props: Props) => {
   return (
     <View>
+      <Text>More actions</Text>
       <Text>MenuItemCard</Text>
+      <Text>{props.menuItem.name}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default MenuItemCard
+export default MenuItemCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/components/home/MenuItemList.tsx
+++ src/components/home/MenuItemList.tsx
@@ -1,12 +1,13 @@
 import { Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import menuItemModel from "../../interfaces/menuItemModel";
+import MenuItemCard from "./MenuItemCard";
 
 export default function MenuItemList() {
   const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
 
   useEffect(() => {
-    fetch("https://cb91-202-28-123-199.ngrok-free.app/api/MenuItem")
+    fetch("https://3436-202-28-123-199.ngrok-free.app/api/MenuItem")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -17,7 +18,10 @@ export default function MenuItemList() {
 
   return (
     <View>
-      <Text>MenuItemList</Text>
+        {menuItems.length > 0 &&
+        menuItems.map((menuItem, index) => (
+          <MenuItemCard menuItem={menuItem} key={index} />
+        ))} 
     </View>
   );
 }
