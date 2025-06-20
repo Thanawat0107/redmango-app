f7fb942995e818849d758cd5101b3d77bd1928bf
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-06-20 10:52:45 +0700

    Section 01 : Navigation and Bottom Tab Navigation

---
+++ src/components/home/MenuItemCard.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MenuItemCard = () => {
+  return (
+    <View>
+      <Text>MenuItemCard</Text>
+    </View>
+  )
+}
+
+export default MenuItemCard
+
+const styles = StyleSheet.create({})
---
+++ src/interfaces/index.ts
@@ -0,0 +1,3 @@
+import menuItemModel from "./menuItemModel";
+
+export default menuItemModel;
