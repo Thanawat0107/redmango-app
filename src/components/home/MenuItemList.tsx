5891fe37f1f44ee1edc12d8d3b7cbe2e8e4874eb
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-06-22 10:44:51 +0700

    Section 01 : Floating Action in Home UI

--- src/components/home/MenuItemList.tsx
+++ src/components/home/MenuItemList.tsx
@@ -1,11 +1,16 @@
 import { FlatList, View } from "react-native";
-import React, { useEffect, useState } from "react";
-import menuItemModel from "../../interfaces/menuItemModel";
+import React, { useEffect, useRef, useState } from "react";
+import menuItemModel from "../../interfaces";
 import MenuItemCard from "./MenuItemCard";
 import styles from "./MenuItemList.style";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { FloatingAction } from "react-native-floating-action";
+import { COLORS } from "../../common/theme";
 
-export default function MenuItemList() {
+const MenuItemList = () => {
   const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+  const flatListRef = useRef<FlatList>(null);
 
   useEffect(() => {
     fetch("https://8a7a-202-28-123-199.ngrok-free.app/api/MenuItem")
@@ -17,9 +22,41 @@ export default function MenuItemList() {
       .catch(() => console.log("error"));
   }, []);
 
+  const actions = [
+    {
+      icon: <AntDesign name="totop" size={24} color={COLORS.white} />,
+      text: "Go to Top",
+      name: "bt_go_to_top",
+      position: 1,
+    },
+    {
+      icon: (
+        <MaterialIcons
+          name="vertical-align-bottom"
+          size={24}
+          color={COLORS.white}
+        />
+      ),
+      text: "Scroll to Bottom",
+      name: "bt_scroll",
+      position: 1,
+    },
+  ];
+
+  const scrollToTop = () => {
+    if (flatListRef.current) {
+      flatListRef.current.scrollToOffset({ animated: true, offset: 0 });
+    }
+  };
+
+  const scrollToBottom = () => {
+    flatListRef.current?.scrollToEnd({ animated: true });
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
+        ref={flatListRef}
         data={menuItems}
         numColumns={2}
         keyExtractor={(item) => item.id.toString()}
@@ -27,6 +64,20 @@ export default function MenuItemList() {
         contentContainerStyle={styles.container}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
       />
+      <FloatingAction
+        distanceToEdge={{ vertical: 20, horizontal: 20 }}
+        actions={actions}
+        onPressItem={(name) => {
+          if (name === "bt_go_to_top") {
+            scrollToTop();
+          }
+
+          if (name === "bt_scroll") {
+            scrollToBottom();
+          }
+        }}
+      />
     </View>
   );
-}
+};
+export default MenuItemList;
