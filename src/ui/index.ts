5a53641306bfe0d7b4f5f5cd34e9c00cd603d414
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-06-22 11:34:04 +0700

    Section 01 : Check server connection Using React Native Paper Dialog (Optional)

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -3,5 +3,6 @@ import BackBtn from "./BackBtn";
 import FormButton from "./FormButton";
 import BackBtn1 from "./BackBtn1";
 import FormButton1 from "./FormButton1";
+import FormDialog from './FormDialog'
 
-export default { FormInput, BackBtn, FormButton, BackBtn1, FormButton1 };
+export { FormInput, BackBtn, FormButton, BackBtn1, FormButton1, FormDialog };
