b43973d3d03c113d1cbb2c7657f55ce5f3c6654d
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-06-23 13:48:17 +0700

    Section 02 : Clean Up Pickup Details

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,5 +1,6 @@
 import getStatusColor from "./getStatusColor";
 import MainLoader from "./MainLoader";
 import { COLORS, SIZES, SHADOWS, FONTS } from "./theme";
+import { PickupDetailsSchema } from "./validator";
 
-export { COLORS, SIZES, SHADOWS, FONTS, MainLoader, getStatusColor };
+export { COLORS, SIZES, SHADOWS, FONTS, MainLoader, getStatusColor, PickupDetailsSchema };
