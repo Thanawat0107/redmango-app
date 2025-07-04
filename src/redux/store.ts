cd80a49efc1d3b260c0f3652f2c27a382651676c
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-07-04 08:57:59 +0700

    Section 04 : Create Payment API

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { menuItemReducer } from "./menuItemSlice";
 import { shoppingCartReducer } from "./shoppingCartSlice";
-import { menuItemApi, shoppingCartApi } from "./apis";
+import { menuItemApi, shoppingCartApi, authApi, paymentApi } from "./apis";
 import { userAuthReducer } from "./userAuthSlice";
-import authApi from "./apis/authApi";
 import devToolsEnhancer from "redux-devtools-expo-dev-plugin";
 
 const store = configureStore({
@@ -15,14 +14,18 @@ const store = configureStore({
     [menuItemApi.reducerPath]: menuItemApi.reducer,
     [shoppingCartApi.reducerPath]: shoppingCartApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [paymentApi.reducerPath]: paymentApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(menuItemApi.middleware)
       .concat(shoppingCartApi.middleware)
-      .concat(authApi.middleware),
-    devTools: false,
-    enhancers: (getDefaultEnhancers) => getDefaultEnhancers().concat(devToolsEnhancer()),
+      .concat(authApi.middleware)
+      .concat(paymentApi.middleware),
+
+  devTools: false,
+  enhancers: (getDefaultEnhancers) =>
+    getDefaultEnhancers().concat(devToolsEnhancer()),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
