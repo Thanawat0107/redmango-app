b78b5bca67d4433486275ee2f29a9122aa8d2a34
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-07-10 12:01:34 +0700

    Section 08 : Design Pagination using React Native Paper

--- src/components/menu/index.ts
+++ src/components/menu/index.ts
@@ -1,7 +1,8 @@
 import MainCard from "./MainCard";
 import MenuCategoryList from "./MenuCategoryList";
 import MenuItemUpsert from "./MenuItemUpsert";
+import MenuPagination from "./MenuPagination";
 import MenuSearchBar from "./MenuSearchBar";
 import MenuSort from "./MenuSort";
 
-export { MainCard, MenuItemUpsert, MenuSearchBar, MenuCategoryList, MenuSort} 
+export { MainCard, MenuItemUpsert, MenuSearchBar, MenuCategoryList, MenuSort, MenuPagination }
