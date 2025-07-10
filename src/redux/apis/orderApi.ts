7b4bb4dc60310d97d50a7dd382f2ea46e5aed9a0
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-07-10 12:18:33 +0700

    Section 08 : Authentication and Authorization End Points

--- src/redux/apis/orderApi.ts
+++ src/redux/apis/orderApi.ts
@@ -1,10 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { baseUrlAPI } from "../../common/SD";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const orderApi = createApi({
   reducerPath: "orderApi",
   baseQuery: fetchBaseQuery({
     baseUrl: baseUrlAPI,
+    prepareHeaders: async (headers: Headers, api) => {
+      const token = await AsyncStorage.getItem("token");
+      token && headers.append("Authorization", "Bearer " + token);
+    },
   }),
   tagTypes: ["Orders"],
   endpoints: (builder) => ({
@@ -20,7 +25,7 @@ const orderApi = createApi({
       invalidatesTags: ["Orders"],
     }),
     getAllOrders: builder.query({
-      query: ({userId,searchString,status,pageNumber,pageSize}) => ({
+      query: ({ userId, searchString, status, pageNumber, pageSize }) => ({
         url: "order",
         params: {
           ...(userId && { userId }),
@@ -46,15 +51,15 @@ const orderApi = createApi({
     }),
     updateOrderHeader: builder.mutation({
       query: (orderDetails) => ({
-          url: "order/" + orderDetails.orderHeaderId,
-          method: "PUT",
-          headers: {
-              "Content-type": "application/json",
-          },
-          body: orderDetails,
+        url: "order/" + orderDetails.orderHeaderId,
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: orderDetails,
       }),
       invalidatesTags: ["Orders"],
-  }),
+    }),
   }),
 });
 
